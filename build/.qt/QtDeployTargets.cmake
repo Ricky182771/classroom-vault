2c23db7ae5247eb0434ee056d2a35004365aa7cd
Ricky182771 <ric4rdo149@gmail.com>
2026-05-12 21:32:17 -0600

    Remove autogenerated files and build artifacts from the classroom-vault project, including moc_predefs.h, mocs_compilation.cpp, timestamp, and cmake_install.cmake.

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_classroom-vault_FILE /home/richi/Documentos/Mis Proyectos/ClassSync/classroom-vault/build/classroom-vault)
-set(__QT_DEPLOY_TARGET_classroom-vault_TYPE EXECUTABLE)
